e655f63a42781fc4c09db8204f0e56b34305d373
Folo4ka <foolkoo@gmail.com>
2024-07-07 13:17:53 +0300

    поиск по термам (без символов в словах)

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,6 +1,7 @@
+import re
+
 from search_engine.types.types import Document
 
 
 def search(docs: list[Document], value: str) -> list[str]:
-    return [doc["id"] for doc in docs if value in doc["text"].split(" ")]
-
+    return [doc["id"] for doc in docs if value in re.findall(r"[a-zA-Z0-9а-яА-я_]+", doc["text"])]
--- tests/test_search.py
+++ tests/test_search.py
@@ -3,20 +3,20 @@ import pytest
 from search_engine.search_engine import search
 
 
+documents = [
+    {"id": "doc1", "text": "I can't shoot straight unless I've had a pint!"},
+    {"id": "doc2", "text": "Don't shoot shoot shoot that thing at me."},
+    {"id": "doc3", "text": "I'm your shooter."},
+]
+
 @pytest.mark.parametrize(
-    argnames=["docs", "expected"],
+    argnames=["docs", "search_value", "expected"],
     argvalues=[
-        (
-            [
-                {'id': 'doc1', 'text': "I can't shoot straight unless I've had a pint!"},
-                {'id': 'doc2', 'text': "Don't shoot shoot shoot that thing at me."},
-                {'id': 'doc3', 'text': "I'm your shooter."},
-            ],
-            ["doc1", "doc2"]
-        ),
-        ([], [])
+        (documents, "shoot", ["doc1", "doc2"]),
+        (documents, "pint", ["doc1"]),
+        ([], "shoot", [])
     ]
 )
-def test_search(docs, expected):
+def test_search(docs, search_value, expected):
     # поисковый движок проводит поиск
-    assert search(docs, "shoot") == expected
+    assert search(docs, search_value) == expected
